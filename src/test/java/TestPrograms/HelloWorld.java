895666b4358ab893e8ac59442112e97401fd903e
MaheshSDET <76080287+MaheshSDET@users.noreply.github.com>
2020-12-15 22:25:19 -0600

    Update HelloWorld.java

--- src/test/java/TestPrograms/HelloWorld.java
+++ src/test/java/TestPrograms/HelloWorld.java
@@ -3,6 +3,7 @@ package TestPrograms;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("welcome to hello world page");
+		System.out.println("welcome to hello world page-2");
 	}
 
 }
